3472a6b944896f66665b47cb8507efcd255bc51d
Edwin Villanueva <edwincv0@gmail.com>
2013-10-31 08:46:55 -0700

    Updated Stopwatch

--- mini_project3.py
+++ mini_project3.py
@@ -1,6 +1,6 @@
 # template for "Stopwatch: The Game"
 # by Edwin Villanueva
-# last updated 10/30/2013
+# last updated 10/31/2013
 
 import simplegui
 import random
@@ -9,9 +9,6 @@ import time
 
 # define global variables
 
-minutes = 0 
-seconds = 0
-milliseconds = 0
 gtime = 0
 
 # define helper function format that converts time
@@ -24,11 +21,20 @@ def new_game():
 
 
     
-    
+# return a string of the form A:BC.D    
 def format(t):
-    global gtime
-    if t:
-        gtime = int(t)
+    t = int(t)
+    D = t % 10
+    C = t / 10 % 10
+    B = t / 100 % 10
+    A = t / 1000 % 10
+    if B > 5:
+        B = B % 6
+        A += 1
+    ftime =  "%d:%d%d.%d" % (A,B, C, D)
+    return ftime
+
+
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start_timer():
@@ -44,18 +50,27 @@ def reset_timer():
     gtime = 0
     print "Timer has been reset..."
 
+def set_custom_time(time):
+    global gtime
+    try:
+        time = int(time)
+        gtime = time
+        print "Time has been updated"
+    except:
+        print "Please enter a valid input =(."
+
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
     global gtime
     gtime += 1
     print int(gtime)
     
-timer = simplegui.create_timer(10, timer_handler)
+timer = simplegui.create_timer(100, timer_handler)
 
 
 # define draw handler
 def draw(canvas):
-    canvas.draw_text(str(int(gtime)), [100, 100], 24, "White")
+    canvas.draw_text(format(gtime), [100, 100], 24, "White")
     
 # create frame
 frame = simplegui.create_frame("Time in'", 200, 200)
@@ -65,7 +80,7 @@ frame.set_draw_handler(draw)
 frame.add_button("Start Timer", start_timer, 200)
 frame.add_button("Stop Timer", stop_timer, 200)
 frame.add_button("Reset Timer", reset_timer, 200)
-frame.add_input("Format", format, 200)
+frame.add_input("Set Custom Time", set_custom_time, 200)
 
 # start frame
 new_game()
